4b851c37763034cd15507223abc051b02df24c69
Ítalo Souza <italo.souza@saude.ce.gov.br>
2019-12-03 11:28:39 -0300

    Change theme to white

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
+      theme: ThemeData(primaryColor: Colors.white),
       home: RandomWords(),
       debugShowCheckedModeBanner: false,
     );
